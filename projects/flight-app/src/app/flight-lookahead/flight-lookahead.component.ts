c9958dd464f9ff05f34614f6c6bbef385ccbf35b
Marco Hämmerle <m.haemmerle@nemocontra.de>
2019-10-31 10:30:14 +0100

    add rxjs demo

--- projects/flight-app/src/app/flight-lookahead/flight-lookahead.component.ts
+++ projects/flight-app/src/app/flight-lookahead/flight-lookahead.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Flight } from '@flight-workspace/flight-api';
-import { combineLatest, interval, Observable } from 'rxjs';
+import { combineLatest, interval, merge, Observable, OperatorFunction, Subject, Subscription } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { debounceTime, distinctUntilChanged, filter, map, startWith, switchMap, tap } from 'rxjs/operators';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter, first,
+  map,
+  startWith,
+  switchMap,
+  take,
+  takeUntil,
+  tap
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-flight-lookahead',
   templateUrl: './flight-lookahead.component.html',
   styleUrls: ['./flight-lookahead.component.css']
 })
-export class FlightLookaheadComponent implements OnInit {
+export class FlightLookaheadComponent implements OnInit, OnDestroy {
   online: boolean = false;
   online$: Observable<boolean>;
 
@@ -18,20 +28,38 @@ export class FlightLookaheadComponent implements OnInit {
 
   flights$: Observable<Flight[]>;
 
+  closeDemoInterval$ = interval(2000).pipe(startWith( ''));
+  destroy$$ = new Subject<void>();
+
   loading: boolean;
 
   constructor(private http: HttpClient) {
   }
 
+  ngOnDestroy(): void {
+    this.destroy$$.next();
+  }
+
   ngOnInit() {
+
+    this.closeDemoInterval$.pipe(takeUntil(this.destroy$$)).subscribe(console.log);
+
+    /* Object destruction
+    const value = { test: 1, test2: 2 };
+    const fun = ({test, test2}) => {
+      test2
+    }
+
+    fun(value);
+    */
+
     this.control = new FormControl();
 
     const input$ =
       this.control
         .valueChanges
         .pipe(
-          debounceTime(300),
-          distinctUntilChanged(),
+          debounceDistinctUntilChanged(300),
           filter(value => value.length >= 3)
         );
 
@@ -43,11 +71,23 @@ export class FlightLookaheadComponent implements OnInit {
       tap(value => this.online = value)
     );
 
+    const obs1 = this.load('Graz');
+    const obs2 = this.load('Stuttgart');
+
+    obs1.pipe(
+      map((flights: Flight[]) => [{
+        ...flights[0],
+        to: flights[0].to,
+        id: 42
+      }])
+    ).subscribe(console.log);
+
     this.flights$ = combineLatest([input$, this.online$])
       .pipe(
-        filter(([, online]) => online),
+        // array destruction
+        filter(([_, online]) => online),
         map(([input]) => input),
-        tap(input => this.loading = true),
+        tap(() => this.loading = true),
         switchMap(input => this.load(input)),
         tap(v => this.loading = false)
       );
@@ -67,3 +107,12 @@ export class FlightLookaheadComponent implements OnInit {
   };
 
 }
+
+const debounceDistinctUntilChanged = (time: number) => {
+  return <T>(source$: Observable<T>) => {
+    return source$.pipe(
+      debounceTime(time),
+      distinctUntilChanged()
+    );
+  };
+};
